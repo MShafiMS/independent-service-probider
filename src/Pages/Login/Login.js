39cda8583ba5db881dcadf5d3ddb201ff35d8e3e
MShafiMS <shahedali734@gmail.com>
2022-04-18 11:01:03 -0600

    password reset

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,13 @@
+import { sendPasswordResetEmail } from "firebase/auth";
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 import SocialLogin from "../SocialLogin/SocialLogin";
 import './Login.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Login = () => {
@@ -19,18 +22,30 @@ const Login = () => {
         user,
         loading,
         error,
-      ] = useSignInWithEmailAndPassword(auth);      const navigate = useNavigate()
+      ] = useSignInWithEmailAndPassword(auth); 
+      const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);     
+      const navigate = useNavigate()
 
       if(user){
           navigate(from, { replace: true});
       }
 
-    const handleSubmit = event => {
+      const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
         signInWithEmailAndPassword(email, password);
+    }
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Sent email');
+        }
+        else{
+            toast('please enter your email address');
+        }
     }
     return (
         <div className="container w-50 mx-auto">
@@ -52,8 +67,10 @@ const Login = () => {
                 Login
             </Button>
             <p>Need an account? <Link to="/register" className='pe-auto text-decoration-none'>Please Register</Link> </p>
+            <p>Forget Password? <button className='btn btn-link text-danger pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p> 
             </Form>
             <SocialLogin></SocialLogin>
+            <ToastContainer />
         </div>
     );
 };
