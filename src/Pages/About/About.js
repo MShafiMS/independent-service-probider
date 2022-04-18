da91bc69a86ea280a174c8ed10b26e3d072e09ea
MShafiMS <shahedali734@gmail.com>
2022-04-18 11:50:36 -0600

    last commit

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Profile from '../../images/profile-1.jpeg';
 
 const About = () => {
     return (
         <div>
-           <h2>This is About section.</h2> 
+           <div classNameName="">
+            <h2 classNameName="title text-center">About me</h2>
+            <div classNameName="d-flex justify-content-center">
+            <img className='w-25' src={Profile} alt="" />
+                <div classNameName="">
+                    <div classNameName="text"><h3 className='text-center'>I'm Shafi and I'm a Photographer</h3><span classNameName="typing-2"></span></div>
+                    <p className='text-center'>Photographers create visual images for an exceptional range of creative, technical and documentary purposes.
+
+                    As a professional photographer, you'll usually work to a brief set by the client or employer.
+
+                    Examples of content you could work on include wedding, family and baby, fashion, food, architecture, and landscape photography.
+
+                    A large proportion of professional photographers are self-employed. You could also find work with a variety of employers, including creative businesses, publishers and photographic agencies, or in the education and public sectors.</p>
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
 
-export default About;<h2>This is About section.</h2>
+export default About;
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Accordion } from 'react-bootstrap';
 
-const Blog = () => {
+const Blogs = () => {
     return (
-        <div>
-          <h2>this is blogs.</h2>  
+        <div className='container'>
+            <Accordion defaultActiveKey={["0"]} alwaysOpen>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header><h3>Difference between Authorization and Authentication</h3></Accordion.Header>
+                    <Accordion.Body>
+                    <span className='fw-bold'>Authentication:</span> Authentication is the process of verifying the identity of a user.Suppose, we have 2 types of users Once we know the user is valid, then (normal and admins) to a
+                    we determine to which pages the user website. When the user tries to has access to. Normal users should not access the website, we ask them to be able to access admin pages. This is log in. This is authentication part. authorization part. <br />
+                    <span className='fw-bold'>Authorization:</span>
+                    Authorization is process of checking whether the user has access rights to the system. Once we know the user is valid, then we determine to which pages the user has acces to. Normal users should not be able to access admin pages. This is authorization part.
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header><h3>Why are you using firebase? What other options do you have to implement authentication?</h3></Accordion.Header>
+                    <Accordion.Body>
+                    Firebase is the best option for Hosting, Authentication, Real time database and other more features. And it is offered by Google. So, I use Firebase. <br />
+
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header><h3>What other services does firebase provide other than authentication</h3></Accordion.Header>
+                    <Accordion.Body>
+                    Without Authentication Firebase Gives us Realtime Database, Hostings, Cloud Messaging, Storage, Remote Config, Test Lab, Crash Reporting etc.
+                    </Accordion.Body>
+                </Accordion.Item>
+                </Accordion>
         </div>
     );
 };
 
-export default Blog;<h2>this is blogs.</h2>
+export default Blogs;
